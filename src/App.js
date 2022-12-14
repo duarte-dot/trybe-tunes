8b78bc3a8ea60ac0df89ee471d6d8ca87347fa69
duarte <gabrieldvr@outlook.com>
2022-12-14 18:39:24 -0300

    requisito 2

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter, Route } from 'react-router-dom';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import Album from './pages/Album';
 import Favorites from './pages/Favorites';
 import Login from './pages/Login';
@@ -7,20 +7,43 @@ import NotFound from './pages/NotFound';
 import Profile from './pages/Profile';
 import ProfileEdit from './pages/ProfileEdit';
 import Search from './pages/Search';
-// import Link from 'react-router-dom';
 
 class App extends React.Component {
   render() {
     return (
       <BrowserRouter>
         <div>
-          <Route path="/profile/edit" component={ ProfileEdit } />
-          <Route path="/profile" component={ Profile } />
-          <Route path="/favorites" component={ Favorites } />
-          <Route path="/album/:id" component={ Album } />
-          <Route path="/search" component={ Search } />
-          <Route exact path="/" component={ Login } />
-          <Route path="*" component={ NotFound } />
+          <Switch>
+            <Route
+              path="/profile/edit"
+              component={ ProfileEdit }
+            />
+            <Route
+              path="/profile"
+              component={ Profile }
+            />
+            <Route
+              path="/favorites"
+              component={ Favorites }
+            />
+            <Route
+              path="/album/:id"
+              component={ Album }
+            />
+            <Route
+              path="/search"
+              component={ Search }
+            />
+            <Route
+              exact
+              path="/"
+              render={ (props) => <Login { ...props } /> }
+            />
+            <Route
+              path="*"
+              component={ NotFound }
+            />
+          </Switch>
         </div>
       </BrowserRouter>
     );
--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,13 +1,90 @@
 import React, { Component } from 'react';
+// import { Redirect } from 'react-router-dom';
+import { PropTypes } from 'prop-types';
+import { createUser } from '../services/userAPI';
+import Loading from './Loading';
 
 class Login extends Component {
+  constructor() {
+    super();
+    this.onInputChange = this.onInputChange.bind(this);
+  }
+
+  state = {
+    name: '',
+    isLoginButtonDisabled: true,
+    isLoggedIn: false,
+  };
+
+  createUserRedirect = (event) => {
+    event.preventDefault();
+    const { history } = this.props;
+    const { name } = this.state;
+
+    this.setState({
+      isLoggedIn: true,
+    }, async () => {
+      await createUser({ name });
+      history.push('/search');
+    });
+  };
+
+  onInputChange = ({ target }) => {
+    const { value } = target;
+    this.setState({
+      name: value,
+    }, () => {
+      const min = 3;
+      if (value.length >= min) {
+        this.setState({
+          isLoginButtonDisabled: false,
+        });
+      } else {
+        this.setState({
+          isLoginButtonDisabled: true,
+        });
+      }
+    });
+  };
+
   render() {
+    const { isLoginButtonDisabled, isLoggedIn } = this.state;
+    if (isLoggedIn) {
+      return <Loading />;
+    }
     return (
       <div data-testid="page-login">
         <h1>Login</h1>
+        <form
+          onSubmit={ this.createUserRedirect }
+        >
+          <label htmlFor="login">
+            Name:
+            <input
+              type="text"
+              id="login"
+              name="login"
+              data-testid="login-name-input"
+              onChange={ this.onInputChange }
+            />
+          </label>
+          <button
+            type="submit"
+            data-testid="login-submit-button"
+            disabled={ isLoginButtonDisabled }
+          >
+            Entrar
+          </button>
+        </form>
       </div>
     );
   }
 }
+
+Login.propTypes = {
+  history: PropTypes.shape({
+    push: PropTypes.func.isRequired,
+  }).isRequired,
+};
 
 export default Login;
