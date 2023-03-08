e0f643c5b1de2b9b6dc8dba85ca9a140a6a016e7
duarte <gabrieldvr@outlook.com>
2023-03-08 10:23:49 -0300

    loading

--- src/components/Loading.js
+++ src/components/Loading.js
@@ -5,6 +5,7 @@ class Loading extends Component {
   render() {
     return (
       <div className="page-loading">
+        <div className="loading" />
         <p>loading...</p>
       </div>
     );
