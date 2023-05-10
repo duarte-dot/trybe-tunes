ef8e64f21360ecb607e0df3c48535fb6ea3e6c9c
duarte <gabrieldvr@outlook.com>
2023-05-10 14:38:37 -0300

    refactor NotFound.js by removing unused test-id and simplifying code

--- src/pages/NotFound.js
+++ src/pages/NotFound.js
@@ -3,9 +3,7 @@ import React, { Component } from 'react';
 class Profile extends Component {
   render() {
     return (
-      <div data-testid="page-not-found">
-        <h1>Page Not Found</h1>
-      </div>
+      <div><h1>Page Not Found</h1></div>
     );
   }
 }
